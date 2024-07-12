333f5989e0e164b19ccdd2f3c73950f7aab19344
Rivio98 <rivaldogjoni1998@gmail.com>
2024-07-12 14:58:51 +0200

    [UPD] Added in the foreach div and published in container

--- js/main.js
+++ js/main.js
@@ -59,4 +59,41 @@ const posts = [
 //dal Dom recupero il contenitore dove andrò a immettere la struttura tramite java-script
 const container = document.getElementById('container')
 
+//effettuo un ciclo forEach per iterare gli oggetti dell'array
+posts.forEach((element) => {
 
+    //creo un div che andrò a inserire all'interno del contenitore
+    container.innerHTML += `<div class="post">
+    <div class="post__header">
+    <div class="post-meta">
+        <div class="post-meta__icon">
+            <img class="profile-pic" src="${element.author.image}" alt="${element.author.name}">
+        </div>
+        <div class="post-meta__data">
+            <div class="post-meta__author">${element.author.name}</div>
+            <div class="post-meta__time">4 mesi fa</div>
+        </div>
+    </div>
+</div>
+<div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad
+    ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+<div class="post__image">
+    <img src="${element.media}" alt="">
+</div>
+<div class="post__footer">
+    <div class="likes js-likes">
+        <div class="likes__cta">
+            <a class="like-button  js-like-button" href="#" data-postid="1">
+                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                <span class="like-button__label">Mi Piace</span>
+            </a>
+        </div>
+        <div class="likes__counter">
+            Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+        </div>
+    </div>
+</div>
+</div >
+</div>`
+
+});
